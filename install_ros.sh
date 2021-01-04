0b5af5b82e172565441c38591d12cc5f7414e50d
Szymon Richert <szymon.rychu@gmail.com>
2021-01-04 13:51:19 +0100

    Add install ros script

--- install_ros.sh
+++ install_ros.sh
@@ -2,8 +2,9 @@
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt update
-sudo apt install ros-noetic-ros-base
+sudo apt-get update
+sudo apt-get install ros-noetic-ros-base -y
+chmod -R ubuntu. /opt/ros/noetic
 echo "source /opt/ros/noetic/setup.zsh" >> ~/.zshrc
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
