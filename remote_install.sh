ed53aa8decbb313fb3f52c4e0bd44f1feba93dbf
Szymon Richert <szymon.rychu@gmail.com>
2021-01-04 14:33:37 +0100

    Add install ros script

--- remote_install.sh
+++ remote_install.sh
@@ -43,19 +43,28 @@ if ! ssh ubuntu@${REMOTE_IP} 'ls ~/catkin_ws > /dev/null'; then
         git clone git@github.com:szymonrychu/ArduinoPlatformController.git;\
         ./ArduinoPlatformController/install_ros.sh;\
         mv ArduinoPlatformController ~/catkin_ws/src/"'
+else
+    ssh ubuntu@${REMOTE_IP} 'bash -c "\
+        cd ~/catkin_ws/src/ArduinoPlatformController;\
+        git stash;\
+        git stash clear;\
+        git checkout master;\
+        git reset --hard;\
+        git clean -xfd;\
+        git pull"'
 fi
 
-if ! ssh ubuntu@${REMOTE_IP} "cat /etc/hosts | grep '${REMOTE_IP} robot'"; then
-    ssh ubuntu@${REMOTE_IP} "echo '${REMOTE_IP} robot' | sudo tee -a /etc/hosts"
+if ! ssh ubuntu@${REMOTE_IP} "cat /etc/hosts | grep '${REMOTE_IP} robot > /dev/null'"; then
+    ssh ubuntu@${REMOTE_IP} "echo '${REMOTE_IP} robot' | sudo tee -a /etc/hosts" > /dev/null
 fi
 
-if ! ssh ubuntu@${REMOTE_IP} "cat /etc/hosts | grep '${LOCAL_IP} overlord'"; then
-    ssh ubuntu@${REMOTE_IP} "echo '${LOCAL_IP} overlord' | sudo tee -a /etc/hosts"
+if ! ssh ubuntu@${REMOTE_IP} "cat /etc/hosts | grep '${LOCAL_IP} overlord > /dev/null'"; then
+    ssh ubuntu@${REMOTE_IP} "echo '${LOCAL_IP} overlord' | sudo tee -a /etc/hosts" > /dev/null
 fi
 
 if ! ssh ubuntu@${REMOTE_IP} 'ls /etc/systemd/system/roscore.service'; then
     ssh ubuntu@${REMOTE_IP} 'bash -c "\
-        cp ~/catkin_ws/src/ArduinoPlatformController/roscore.service /etc/systemd/system/roscore.service;\
+        sudo cp ~/catkin_ws/src/ArduinoPlatformController/roscore.service /etc/systemd/system/roscore.service;\
         sudo systemctl enable roscore;\
         sudo systemctl start roscore"'
 fi
@@ -63,4 +72,11 @@ fi
 ssh ubuntu@${REMOTE_IP} 'bash -c "\
     source /opt/ros/noetic/setup.bash;\
     cd ~/catkin_ws;\
-    catkin_make -DCMAKE_INSTALL_PREFIX=/opt/ros/noetic install"'
+    catkin_make -DCMAKE_INSTALL_PREFIX=/opt/ros/noetic install;\"'
+
+if ! ssh ubuntu@${REMOTE_IP} 'ls /etc/systemd/system/ros_platform.service'; then
+    ssh ubuntu@${REMOTE_IP} 'bash -c "\
+        sudo cp ~/catkin_ws/src/ArduinoPlatformController/ros_platform.service /etc/systemd/system/ros_platform.service;\
+        sudo systemctl enable ros_platform;\
+        sudo systemctl restart ros_platform"'
+fi
