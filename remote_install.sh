64ff86093331dbf6e5a3555f12c268c6d94d7509
Szymon Richert <szymon.rychu@gmail.com>
2021-01-04 14:28:52 +0100

    Add install ros script

--- remote_install.sh
+++ remote_install.sh
@@ -4,6 +4,12 @@ if [ -z $REMOTE_IP ]; then
     echo "Remote IP missing!" 1>&2
     exit 1
 fi
+if ! cat /etc/hosts | grep "${REMOTE_IP} robot" > /dev/null; then
+    echo "${REMOTE_IP} robot" | sudo tee -a /etc/hosts
+fi
+ssh-keyscan -t rsa ${REMOTE_IP} >> ~/.ssh/known_hosts
+
+LOCAL_IP=$(hostname -I | awk '{print $1}')
 
 if ! ssh -o PasswordAuthentication=no  -o BatchMode=yes ubuntu@${REMOTE_IP} exit &>/dev/null; then
     echo "Ensuring ssh keys are present on remote."
@@ -39,3 +45,22 @@ if ! ssh ubuntu@${REMOTE_IP} 'ls ~/catkin_ws > /dev/null'; then
         mv ArduinoPlatformController ~/catkin_ws/src/"'
 fi
 
+if ! ssh ubuntu@${REMOTE_IP} "cat /etc/hosts | grep '${REMOTE_IP} robot'"; then
+    ssh ubuntu@${REMOTE_IP} "echo '${REMOTE_IP} robot' | sudo tee -a /etc/hosts"
+fi
+
+if ! ssh ubuntu@${REMOTE_IP} "cat /etc/hosts | grep '${LOCAL_IP} overlord'"; then
+    ssh ubuntu@${REMOTE_IP} "echo '${LOCAL_IP} overlord' | sudo tee -a /etc/hosts"
+fi
+
+if ! ssh ubuntu@${REMOTE_IP} 'ls /etc/systemd/system/roscore.service'; then
+    ssh ubuntu@${REMOTE_IP} 'bash -c "\
+        cp ~/catkin_ws/src/ArduinoPlatformController/roscore.service /etc/systemd/system/roscore.service;\
+        sudo systemctl enable roscore;\
+        sudo systemctl start roscore"'
+fi
+
+ssh ubuntu@${REMOTE_IP} 'bash -c "\
+    source /opt/ros/noetic/setup.bash;\
+    cd ~/catkin_ws;\
+    catkin_make -DCMAKE_INSTALL_PREFIX=/opt/ros/noetic install"'
