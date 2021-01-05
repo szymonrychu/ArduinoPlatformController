24f84fc0dc479c95777acf46b38dd391120ab741
Szymon Richert <szymon.rychu@gmail.com>
2021-01-05 13:53:40 +0100

    Bugfix

--- ros_omni_platform/src/omni_platform/platform_statics.py
+++ ros_omni_platform/src/omni_platform/platform_statics.py
@@ -28,7 +28,7 @@ class PlatformStatics():
     @staticmethod
     def wheel0_RPY(R, P, Y):
         # return 0, P, math.pi + math.pi/2 + Y
-        return 0, P, - Y + PlatformStatics.ANGLE_OFFSETS[0]
+        return 0, P, Y + PlatformStatics.ANGLE_OFFSETS[0]
     @staticmethod
     def wheel1_RPY(R, P, Y):
         # return 0, P, math.pi + math.pi/2 - Y
@@ -36,7 +36,7 @@ class PlatformStatics():
     @staticmethod
     def wheel2_RPY(R, P, Y):
         # return 0, P, math.pi + math.pi/2 - Y
-        return 0, P, - Y + PlatformStatics.ANGLE_OFFSETS[2]
+        return 0, P, Y + PlatformStatics.ANGLE_OFFSETS[2]
     @staticmethod
     def wheel3_RPY(R, P, Y):
         # return 0, P, math.pi + math.pi/2 + Y
