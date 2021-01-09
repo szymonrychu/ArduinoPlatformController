3d46e38ca05617f8cdfa00de30867c4fb16c7aa9
Szymon Richert <szymon.rychu@gmail.com>
2021-01-09 14:57:25 +0100

    Bugfix

--- ros_omni_platform/src/omni_platform/tf2_publisher.py
+++ ros_omni_platform/src/omni_platform/tf2_publisher.py
@@ -165,7 +165,7 @@ class TF2Platform(TF2Link):
                 
                 centre_x = (xyz_s[0][0] + xyz_s[1][0] + xyz_s[2][0] + xyz_s[3][0])/4
                 centre_y = (xyz_s[0][1] + xyz_s[1][1] + xyz_s[2][1] + xyz_s[3][1])/4
-                Y = math.atan2(fm_point[0]-bm_point[0], fm_point[1]-bm_point[1])
+                Y = math.atan2(bm_point[0] - fm_point[0], bm_point[1] - fm_point[1])
 
                 rospy.loginfo(f"wheels [{xyz_s[0][0]}, {xyz_s[0][1]}][{xyz_s[1][0]}, {xyz_s[1][1]}][{xyz_s[2][0]}, {xyz_s[2][1]}][{xyz_s[3][0]}, {xyz_s[3][1]}]")
                 rospy.loginfo(f"fm/bm/centre/yaw [{fm_point[0]}, {fm_point[1]}][{bm_point[0]}, {bm_point[1]}][{centre_x}, {centre_y}] {Y}")
