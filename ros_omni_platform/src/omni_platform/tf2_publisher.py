cc9a790416ce3dee042624b46bc5bc320b79a9b3
Szymon Richert <szymon.rychu@gmail.com>
2021-01-09 15:39:49 +0100

    Bugfix

--- ros_omni_platform/src/omni_platform/tf2_publisher.py
+++ ros_omni_platform/src/omni_platform/tf2_publisher.py
@@ -162,7 +162,7 @@ class TF2Platform(TF2Link):
 
                 Y = - math.atan2(10000 * (front_x - centre_x), -10000 * (front_y - centre_y))
 
-                rospy.loginfo(f"front/centre/yaw [{front_x}, {front_y}][{centre_x}, {centre_y}] {math.degrees(Y)}")
+                rospy.loginfo(f"dx-dy/yaw [{front_x - centre_x}, {front_y - centre_y}] {math.degrees(Y)}")
                 self._tf_broadcaster.sendTransform(self.update(centre_x, centre_y, 0, 0, 0, Y, increment=False)) # self.update_Y(Y)
 
 
