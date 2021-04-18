0b7ffca642ca0384891ce3f16ed215720ead8b7e
Szymon Richert <szymon.rychu@gmail.com>
2021-04-18 20:51:17 +0200

    Bugfix

--- ros_omni_platform/src/omni_platform/controller.py
+++ ros_omni_platform/src/omni_platform/controller.py
@@ -64,12 +64,12 @@ class PlatformController(PlatformMath, PlatformCommands):
         angles = list(self.get_in_place_angles())
         distances = list(self.get_in_place_angle2distance(angle))
 
-        for wheel_thread, wheel_angle in zip(self.__threads, angles):
+        for wheel_id, (wheel_thread, wheel_angle) in enumerate(zip(self.__threads, angles)):
             move_command = self.move_command(wheel_angle, 0.0, 5)
             rospy.loginfo("{}: {}".format(str(wheel_id+1), move_command))
             wheel_thread.write_data(move_command)
         time.sleep(1)
-        for wheel_thread, wheel_angle, wheel_distance in zip(self.__threads, angles, distances):
+        for wheel_id, (wheel_thread, wheel_angle, wheel_distance) in enumerate(zip(self.__threads, angles, distances)):
             move_command = self.move_command(wheel_angle, wheel_distance, moving_time)
             rospy.loginfo("{}: {}".format(str(wheel_id+1), move_command))
             wheel_thread.write_data(move_command)
