66c4da05df7ec1e53107993250b6bb542d70329f
Szymon Richert <szymon.rychu@gmail.com>
2021-04-18 17:56:44 +0200

    Bugfix

--- ros_omni_platform/src/omni_platform/platform_math.py
+++ ros_omni_platform/src/omni_platform/platform_math.py
@@ -84,7 +84,7 @@ class PlatformMath(PlatformStatics):
         
     def get_in_place_angles(self):
         base_angle = math.atan(PlatformMath.ROBOT_LENGTH/PlatformMath.ROBOT_WIDTH)
-        return base_angle, -base_angle, base_angle, -base_angle
+        return base_angle, -base_angle, -base_angle, base_angle
 
 
 
