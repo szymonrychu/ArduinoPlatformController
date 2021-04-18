3e64aec1b2824cc978f5cb98fa85a9897d1f4ca1
Szymon Richert <szymon.rychu@gmail.com>
2021-04-18 21:30:53 +0200

    Bugfix

--- ros_omni_platform/src/omni_platform/omni_platform.py
+++ ros_omni_platform/src/omni_platform/omni_platform.py
@@ -52,7 +52,7 @@ class OmniPlatform(PlatformController):
 
         r, p, y = tf_conversions.transformations.euler_from_quaternion([data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w])
         rospy.loginfo(f"x,y,z,r,p,y:{dx},{dy},{dz},{r},{p},{y}")
-        self.turn_in_place(math.pi/4, 30000)
+        self.turn_in_place(math.pi/4, 3000)
 
     def start(self):
         rospy.spin()
