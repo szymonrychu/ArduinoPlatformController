1ee6c39c1ae9a04d6a6a0e3448c19667f4b54589
Szymon Richert <szymon.richert@dynatrace.com>
2022-04-01 14:45:37 +0200

    Add stateful logic

--- platformio/wheel_bridge/src/main.cpp
+++ platformio/wheel_bridge/src/main.cpp
@@ -33,8 +33,16 @@ float distanceVelocityTarget = 0;
 
 long distancePower = 0;
 long anglePower = 0;
+
 bool optoPrevState = false;
 bool optoResetSuccessfull = false;
+
+#define WHEEL_STATE_FRESH           0
+#define WHEEL_STATE_RESET_REQUESTED 1
+#define WHEEL_STATE_ACCEPTING_CMDS  2
+#define WHEEL_STATE_BUSY            4
+
+int wheelState = WHEEL_STATE_FRESH;
 
 #include "commands.h"
 
@@ -46,6 +54,7 @@ void serialEvent() {
 }
 
 void reset(){
+    wheelState = WHEEL_STATE_FRESH;
     angleEncoder.setup(ENCODER_TO_ANGLE);
     distanceEncoder.setup(ENCODER_TO_DISTANCE);
     angleEncoder.start();
@@ -54,25 +63,6 @@ void reset(){
     distanceEncoder.zeroFTM();
     anglePIDs.setup(ANGLE_DPID_P, ANGLE_DPID_I, ANGLE_DPID_D, ANGLE_VPID_P, ANGLE_VPID_I, ANGLE_VPID_D);
     distancePIDs.setup(DISTANCE_DPID_P, DISTANCE_DPID_I, DISTANCE_DPID_D, DISTANCE_VPID_P, DISTANCE_VPID_I, DISTANCE_VPID_D);
-    optoResetSuccessfull = true;
-}
-
-void optoInterruptHandler(){
-    if(optoPrevState && digitalRead(13) == LOW){
-        angle.drive(0);
-        detachInterrupt(digitalPinToInterrupt(13));
-        reset();
-        optoPrevState = false;
-    }else if(!optoPrevState && digitalRead(13) == HIGH){
-        optoPrevState = true;
-    }
-}
-
-void requestAngleZeroing(){
-    angle.drive(pow(2, PWM_RESOLUTION)*ZEROING_RELATIVE_POWER);
-    delay(100);
-    optoResetSuccessfull = false;
-    attachInterrupt(digitalPinToInterrupt(13), optoInterruptHandler, CHANGE);
 }
 
 void setup(){
@@ -82,42 +72,67 @@ void setup(){
     angle.setup();
     distance.setup();
     while(Serial){;} // Wait for someone to open Serial port
-    requestAngleZeroing();
 }
 
 void printDiagnostics(){
-    sprintf(buffer, "%.4f:%.4f:%.4f:%d %.4f:%.4f:%.4f:%d %.3f", 
+    sprintf(buffer, "%d %.4f:%.4f:%.4f:%d %.4f:%.4f:%.4f:%d %.3f", wheelState,
         angleEncoder.getLastPosition(), anglePIDs.getError(), angleEncoder.getLastVelocity()*1000.0*1000.0, anglePower,
         distanceEncoder.getLastPosition(), distancePIDs.getError(), distanceEncoder.getLastVelocity()*1000.0*1000.0, distancePower,
         timeDelta*1000.0);
     Logger::info(buffer);
 }
 
+void optoInterruptHandler(){
+    if(optoPrevState && digitalRead(13) == LOW){
+        angle.drive(0);
+        detachInterrupt(digitalPinToInterrupt(13));
+        reset();
+        optoPrevState = false;
+        wheelState = WHEEL_STATE_ACCEPTING_CMDS;
+    }else if(!optoPrevState && digitalRead(13) == HIGH){
+        optoPrevState = true;
+    }
+}
+
 void loop(){
-    if(optoResetSuccessfull){
-        unsigned long currentTime = micros();
-        timeDelta = (float)(currentTime - lastLoopTime)/1000000.0;
-        lastLoopTime = currentTime;
+    unsigned long currentTime = micros();
+    timeDelta = (float)(currentTime - lastLoopTime)/1000000.0;
+    lastLoopTime = currentTime;
 
-        command.parse();
+    switch(wheelState){
+        case WHEEL_STATE_FRESH:
+            angle.drive(pow(2, PWM_RESOLUTION)*ZEROING_RELATIVE_POWER);
+            delay(100);
+            attachInterrupt(digitalPinToInterrupt(13), optoInterruptHandler, CHANGE);
+            wheelState = WHEEL_STATE_RESET_REQUESTED;
+            break;
+        case WHEEL_STATE_RESET_REQUESTED:
+            delay(1);
+            break;
+        case WHEEL_STATE_ACCEPTING_CMDS:
+        case WHEEL_STATE_BUSY:
 
-        angleEncoder.compute();
-        distanceEncoder.compute();
+            command.parse();
 
-        distancePower = distancePIDs.computeSteering(distanceTarget, distanceVelocityTarget, (timeDelta/1000.0));
-        anglePower = anglePIDs.computeSteering(angleTarget, angleVelocityTarget, (timeDelta/1000.0));
-        
-        distance.drive(distancePower);
-        angle.drive(anglePower);
+            angleEncoder.compute();
+            distanceEncoder.compute();
 
-        if(currentTime - lastPrintTime > 100.0*1000.0){
-            printDiagnostics();
-            lastPrintTime = currentTime;
-        }
+            distancePower = distancePIDs.computeSteering(distanceTarget, distanceVelocityTarget, (timeDelta/1000.0));
+            anglePower = anglePIDs.computeSteering(angleTarget, angleVelocityTarget, (timeDelta/1000.0));
+            
+            distance.drive(distancePower);
+            angle.drive(anglePower);
 
-        distancePIDs.setResults(distanceEncoder.getLastPosition(), distanceEncoder.getLastVelocity());
-        anglePIDs.setResults(angleEncoder.getLastPosition(), angleEncoder.getLastVelocity());
-    }else{
-        delay(1);
+            distancePIDs.setResults(distanceEncoder.getLastPosition(), distanceEncoder.getLastVelocity());
+            anglePIDs.setResults(angleEncoder.getLastPosition(), angleEncoder.getLastVelocity());
+            break;
+        default:
+            delay(1);
+            break;
+    }
+
+    if(currentTime - lastPrintTime > 100.0*1000.0){
+        printDiagnostics();
+        lastPrintTime = currentTime;
     }
 }
