0c59fe1803016e9a9e7e419fd2aaa3a8d418a44c
Szymon Richert <szymon.rychu@gmail.com>
2022-05-04 16:42:16 +0200

    Add more logging for function receiving commands

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,7 @@
 source /opt/ros/noetic/setup.bash
 ROS_HOSTNAME=robot
 ROS_MASTER_URI=http://robot:11311
+ROS_DISTRO=noetic
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 "${SCRIPT_DIR}/refresh.sh"
