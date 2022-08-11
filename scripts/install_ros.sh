195bef09cf77db153274423fc55fb43b7c66652f
Szymon Richert <szymon.rychu@gmail.com>
2022-08-11 21:49:45 +0200

    Update install_ros.sh

--- scripts/install_ros.sh
+++ scripts/install_ros.sh
@@ -3,8 +3,9 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
-sudo apt-get install ros-noetic-ros-base -y
-sudo chown -R ubuntu. /opt/ros/noetic
+sudo apt-get install ros-noetic-ros-base ros-noetic-camera-info-manager ros-noetic-driver-base libfreenect-dev -y
+
+sudo chown -R ${USER}. /opt/ros/noetic
 echo "source /opt/ros/noetic/setup.zsh" >> ~/.zshrc
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source /opt/ros/noetic/setup.bash
