7981ebe1aedb7fb9bf2eb09e425362ac7e347909
Szymon Richert <szymon.rychu@gmail.com>
2023-07-08 22:38:35 +0200

    Add docker container for small robot

--- ros/small_robot/src/small_robot/small_robot.py
+++ ros/small_robot/src/small_robot/small_robot.py
@@ -4,8 +4,11 @@ import math
 import signal
 
 from geometry_msgs.msg import PoseStamped, Pose, TransformStamped, Point
+from sensor_msgs.msg import BatteryState
 from tf2_ros import TransformBroadcaster
+from std_msgs.msg import Bool
 import tf_conversions
+import json
 
 
 from .lib import env2log, ROBOT_WIDTH_M, Rate, SupressedLog, RobotQuaternion, SerialWrapper
@@ -18,6 +21,9 @@ class RobotPlatform():
         self.__serial_dev = rospy.get_param('~serial_dev')
         self.__serial_baudrate = rospy.get_param('~serial_baudrate')
 
+        self.__state_ready_publisher = rospy.Publisher(rospy.get_param("~state_ready_topic"), Bool, queue_size=10)
+        self.__battery_state_publisher = rospy.Publisher(rospy.get_param("~battery_state_topic"), Bool, queue_size=10)
+
         self.__serial = SerialWrapper(self.__serial_dev, self.__serial_baudrate)
         rospy.Timer(rospy.Duration(0.001), self.parse_serial)
 
@@ -25,6 +31,20 @@ class RobotPlatform():
         raw_data = self.__serial.read_data()
         if not raw_data:
             return
+        data = json.loads(raw_data)
+
+        
+        
+        state = Bool()
+        state.data = data['state']
+        self.__state_ready_publisher.publish(state)
+        battery = BatteryState()
+        battery.voltage = data['battery']['voltage']
+        battery.present = True
+        battery.power_supply_technology = 2
+        battery.power_supply_status = 2 # 1=charging 2=discharging 3=not_charging 4=full
+        self.__battery_state_publisher.publish(battery)
+
         rospy.loginfo(raw_data)
 
     def start(self):
