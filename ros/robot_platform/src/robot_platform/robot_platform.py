e77ff2bb90e3626da34c0a57b557a4464e38f941
Szymon Richert <szymon.rychu@gmail.com>
2023-08-16 12:51:14 +0200

    Add testing covariance matrix handling

--- ros/robot_platform/src/robot_platform/robot_platform.py
+++ ros/robot_platform/src/robot_platform/robot_platform.py
@@ -81,6 +81,7 @@ class RobotPlatformRawSerialROSNode(SerialROSNode):
         self._map_frame_id = rospy.get_param('~map_frame_id')
         self._robot_to_map_projection_frame_id = rospy.get_param('~robot_to_map_projection_frame_id')
         self._robot_frame_id = rospy.get_param('~robot_frame_id')
+        self._rplidar_frame_id = rospy.get_param('~rplidar_frame_id')
 
         raw_input_topic = rospy.get_param('~raw_input_topic')
         rospy.Subscriber(raw_input_topic, String, self._write_raw_data)
@@ -150,6 +151,7 @@ class RobotPlatformRawSerialROSNode(SerialROSNode):
         self._imu_state_publisher.publish(status.parse_ROS_IMU(self._robot_frame_id))
         self._tf_broadcaster.sendTransform(create_static_transform(self._map_frame_id, self._robot_to_map_projection_frame_id, Z=0.1))
         self._tf_broadcaster.sendTransform(status.parse_ROS_TF(self._robot_to_map_projection_frame_id, self._robot_frame_id))
+        self._tf_broadcaster.sendTransform(create_static_transform(self._robot_frame_id, self._rplidar_frame_id, Y=0.1, Z=0.1))
 
     def __handle_goal_pose_input_data(self, goal_pose:Pose):
         pose_difference = difference_between_Poses(self._current_pose, goal_pose)
