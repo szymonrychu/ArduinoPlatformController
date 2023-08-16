3f9f849de1c3b066157f16d7c70722d26b67c216
Szymon Richert <szymon.rychu@gmail.com>
2023-08-16 10:36:36 +0200

    Add testing covariance matrix handling

--- ros/robot_platform/src/robot_platform/robot_platform.py
+++ ros/robot_platform/src/robot_platform/robot_platform.py
@@ -3,6 +3,8 @@
 import rospy
 import signal
 
+import numpy as np
+
 from std_msgs.msg import String
 from geometry_msgs.msg import Pose
 from sensor_msgs.msg import BatteryState, NavSatFix, NavSatStatus, Imu
@@ -143,17 +145,33 @@ class RobotPlatformRawSerialROSNode(SerialROSNode):
         imu.orientation.x = status.quaternion.x
         imu.orientation.y = status.quaternion.y
         imu.orientation.z = status.quaternion.z
-        # imu.orientation_covariance
+        imu.orientation_covariance = np.cov([
+            imu.orientation.w,
+            imu.orientation.x,
+            imu.orientation.y,
+            imu.orientation.z
+        ])
+
 
         imu.angular_velocity.x = status.gyroscope.x
         imu.angular_velocity.y = status.gyroscope.y
         imu.angular_velocity.z = status.gyroscope.z
-        # imu.angular_velocity_covariance
+        imu.angular_velocity_covariance = np.cov([
+            imu.angular_velocity.x,
+            imu.angular_velocity.y,
+            imu.angular_velocity.z
+        ])
 
         imu.linear_acceleration.x = status.accelerometer.x
         imu.linear_acceleration.y = status.accelerometer.y
         imu.linear_acceleration.z = status.accelerometer.z
-        # imu.linear_acceleration_covariance
+        imu.linear_acceleration_covariance = np.cov([
+            imu.linear_acceleration.x,
+            imu.linear_acceleration.y,
+            imu.linear_acceleration.z
+        ])
+        
+        print(imu.orientation_covariance)
 
         self._imu_state_publisher.publish(imu)
 
