2dcfec6f30edb4697df0362f10135dcaa35e4c54
Szymon Richert <szymon.rychu@gmail.com>
2023-12-08 10:55:40 +0000

    Fix code srvice

--- code.sh
+++ code.sh
@@ -3,13 +3,11 @@
 mkdir -p "${HOME}/config"
 
 docker run --rm -p 8080:8080 \
-  --privileged \
   -v "$HOME/.local:/home/coder/.local" \
   -v "$HOME/.config:/home/coder/.config" \
   -v "$HOME/.ssh:/home/coder/.ssh" \
   -v "$HOME/.gitconfig:/home/coder/.gitconfig" \
   -v "$HOME/.bash_history:/home/coder/.bash_history" \
-  -v "/opt/ros/noetic:/opt/ros/noetic" \
   -v "$HOME/ArduinoPlatformController:/home/coder/ArduinoPlatformController" \
   -u "$(id -u):$(id -g)" \
   --add-host robot:192.168.1.48 \
