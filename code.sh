fcc25816398ca7d746c0d4f86a7385864b7ae0f2
Szymon Richert <szymon.rychu@gmail.com>
2023-12-08 09:50:28 +0000

    Update code server starting script

--- code.sh
+++ code.sh
@@ -6,7 +6,9 @@ docker run --rm -p 8080:8080 \
   -v "$HOME/.local:/home/coder/.local" \
   -v "$HOME/.config:/home/coder/.config" \
   -v "$HOME/.ssh:/home/coder/.ssh" \
-  -v "/opt/ros/noetic:/opt/ros/noetic" \
+  -v "$HOME/.git/coder/.git" \
+  -v "/opt/ros/noetic" \
+  -v "/:/root" \
   -v "$HOME/ArduinoPlatformController:/home/coder/ArduinoPlatformController" \
   -u "$(id -u):$(id -g)" \
   -e "DOCKER_USER=$USER" \
