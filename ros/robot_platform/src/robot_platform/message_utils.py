4d55602d3ac7a771f91a5cbcc2cd8160cdc1cad8
Szymon Richert <szymon.rychu@gmail.com>
2024-04-04 14:05:11 +0200

    Simplify messages for odometry_handler

--- ros/robot_platform/src/robot_platform/message_utils.py
+++ ros/robot_platform/src/robot_platform/message_utils.py
@@ -164,9 +164,9 @@ class StatusResponse(Message):
         platform_status.motor4.servo.angle = self.motor4.angle
         platform_status.pan.angle = self.pan.angle
         platform_status.tilt.angle = self.tilt.angle
-        platform_status.imu = self.imu.parse_ROS_IMU(header, timestamp)
-        platform_status.gps = self.gps.parse_ROS_GPS(header, timestamp)
-        platform_status.battery = self.battery.parse_ROS_Battery(header, timestamp)
+        platform_status.imu = self.imu.parse_ROS_IMU(header_frame_id, timestamp)
+        platform_status.gps = self.gps.parse_ROS_GPS(header_frame_id, timestamp)
+        platform_status.battery = self.battery.parse_ROS_Battery(header_frame_id, timestamp)
         return platform_status
 
 class Request(Message):
