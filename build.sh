80a2cd58321f88f8c1923da692334ab08d9ac439
Szymon Richert <szymon.rychu@gmail.com>
2024-04-12 20:59:29 +0200

    Fixes

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ readonly GIT_REPO_ROOT="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 cd "${GIT_REPO_ROOT}"
 
+git add -A
+git stash
+git stash drop
+
 readonly GIT_REPO_SHORT_SHA="$(git rev-parse HEAD)"
 
 docker build \
