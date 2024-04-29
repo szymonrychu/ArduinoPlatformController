6f44b235a28c9c9181a3d671b3015cf013ba8187
Szymon Richert <szymon.rychu@gmail.com>
2024-04-29 10:03:30 +0200

    Update

--- ros/robot_platform/src/robot_platform/wheel_controller.py
+++ ros/robot_platform/src/robot_platform/wheel_controller.py
@@ -123,7 +123,7 @@ class WheelController(ROSNode, SafeSerialWrapper):
 
             transforms = [
                 create_static_transform(self._base_footprint_frame_id, self._base_frame_id, 0.0, 0.0, 0.07, 0, 0, 0, rospy_time_now),
-                create_static_transform(self._base_frame_id, self._laser_frame_id, 0.0, 0.08, 0.14, 0, 0, -math.pi/2, rospy_time_now),
+                create_static_transform(self._laser_frame_id, self._base_frame_id, 0.0, -0.08, -0.14, 0, 0, -math.pi/2, rospy_time_now),
                 create_static_transform(self._base_frame_id, self._camera_frame_id, 0.0, 0.0, 0, 0, 0, 0, rospy_time_now)
             ]
             mean_distance_delta = sum([m.distance for m in response.motor_list]) / len(response.motor_list)
