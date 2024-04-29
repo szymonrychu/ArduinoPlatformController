463280e8fcf75c59ce36b535703895df06003f5a
Szymon Richert <szymon.rychu@gmail.com>
2024-04-29 13:31:42 +0200

    Update

--- ros/robot_platform/src/robot_platform/wheel_controller.py
+++ ros/robot_platform/src/robot_platform/wheel_controller.py
@@ -135,8 +135,8 @@ class WheelController(ROSNode, SafeSerialWrapper):
 
             transforms.append(create_static_transform(self._base_frame_id, self._computed_turning_point_frame_id, computed_turning_point.x, computed_turning_point.y, 0, 0, 0, 0, rospy_time_now))
         
-        self._total_X -= mean_distance_delta * math.asin(self._total_yaw)
-        self._total_Y += mean_distance_delta * math.acos(self._total_yaw)
+        self._total_X -= mean_distance_delta * math.sin(self._total_yaw)
+        self._total_Y += mean_distance_delta * math.cos(self._total_yaw)
 
         odometry = Odometry()
         odometry.header.stamp = rospy_time_now
