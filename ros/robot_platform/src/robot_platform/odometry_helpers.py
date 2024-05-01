6f8653c874b96d8c1dd04ee8bbeb6bfaa251dca2
Szymon Richert <szymon.rychu@gmail.com>
2024-05-01 20:17:44 +0200

    Update

--- ros/robot_platform/src/robot_platform/odometry_helpers.py
+++ ros/robot_platform/src/robot_platform/odometry_helpers.py
@@ -20,22 +20,22 @@ class PlatformStatics:
     # LEFT BACK
     M1_IN_PLACE_TURN = -1.0
     M1_X = -(ROBOT_WIDTH/2) 
-    M1_Y = -(ROBOT_LENGTH/2)
+    M1_Y = (ROBOT_LENGTH/2)
 
     # RIGHT BACK
     M2_IN_PLACE_TURN = -1.0
-    M2_X = (ROBOT_WIDTH/2) 
+    M2_X = -(ROBOT_WIDTH/2) 
     M2_Y = -(ROBOT_LENGTH/2)
 
     # RIGHT FRONT
     M3_IN_PLACE_TURN = -1.0
     M3_ANGLE_COEFFICIENT = 1.0
     M3_X = (ROBOT_WIDTH/2) 
-    M3_Y = (ROBOT_LENGTH/2)
+    M3_Y = -(ROBOT_LENGTH/2)
 
     # LEFT FRONT
     M4_IN_PLACE_TURN = -1.0
-    M4_X = -(ROBOT_WIDTH/2) 
+    M4_X = (ROBOT_WIDTH/2) 
     M4_Y = (ROBOT_LENGTH/2)
 
     ROBOT_MOTORS_DIMENSIONS = [
