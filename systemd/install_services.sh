3f89319ef7401fd29bd557148a47baaab0a2eb60
Szymon Richert <szymon.rychu@gmail.com>
2024-07-11 23:14:18 +0200

    Add ability to shutdown system from ros

--- systemd/install_services.sh
+++ systemd/install_services.sh
@@ -5,15 +5,16 @@ readonly THIS_ROOT="$(dirname "${THIS_FULL_PATH}")"
 
 cd "${THIS_ROOT}"
 
-sed -e "s/SCRIPT_FULL_PATH/${THIS_ROOT//\//\\/}/g" ./ros.service > /tmp/ros.service
 
 service_updated=""
 
+sed -e "s/SCRIPT_FULL_PATH/${THIS_ROOT//\//\\/}/g" ./ros.service > /tmp/ros.service
 if ! diff /tmp/ros.service /etc/systemd/system/ros.service; then
   sudo mv /tmp/ros.service /etc/systemd/system/ros.service
   service_updated="${service_updated}Y"
 fi
 
+sed -e "s/SCRIPT_FULL_PATH/${THIS_ROOT//\//\\/}/g" ./shutdown.service > /shutdown/ros.service
 if ! diff /tmp/shutdown.service /etc/systemd/system/shutdown.service; then
   sudo mv /tmp/shutdown.service /etc/systemd/system/shutdown.service
   service_updated="${service_updated}Y"
