06d1e242d20fb9649953f043f10c2ab4c4efc9a1
Szymon Richert <szymon.rychu@gmail.com>
2024-07-19 11:39:53 +0200

    Update overlord IP

--- ros/robot_platform/src/robot_platform/path_controller.py
+++ ros/robot_platform/src/robot_platform/path_controller.py
@@ -8,8 +8,8 @@ from .odometry_helpers import *
 import rospy
 from .ros_helpers import ROSNode
 
-from robot_platform.msg import MoveRequest
-from geometry_msgs.msg import Twist
+from robot_platform.msg import PlatformStatus, MoveRequest
+from geometry_msgs.msg import PoseArray, Twist
 from nav_msgs.msg import Odometry
 
 duration = 0.1
@@ -18,14 +18,27 @@ class PathPlatformController(ROSNode):
 
     def __init__(self):
         ROSNode.__init__(self)
-        self._busy = False
+        self._last_platform_status = PlatformStatus()
+
+        self._last_odometry = Odometry()
+
+        self._last_pose_array = PoseArray()
+        self._pose_counter = 1
+        self._last_angle = 0.0
 
         move_request_output_topic = rospy.get_param('~move_request_output_topic')
+        platform_status_input_topic = rospy.get_param('~platform_status_input_topic')
+        
         self._move_request_publisher = rospy.Publisher(move_request_output_topic, MoveRequest)
+        rospy.Subscriber(platform_status_input_topic, PlatformStatus, self._handle_platform_status)
         rospy.Subscriber('/cmd_vel', Twist, self._handle_trajectory_update)
 
-        # rospy.Timer(rospy.Duration(duration), self._send_request)
+        self._tf_buffer = tf2_ros.Buffer()
+        self._tf_listener = tf2_ros.TransformListener(self._tf_buffer)
+
+        rospy.Timer(rospy.Duration(duration), self._send_request)
         self.spin()
+
 
     def _handle_trajectory_update(self, cmd_vel:Twist):
         forward = cmd_vel.linear.x
@@ -37,6 +50,8 @@ class PathPlatformController(ROSNode):
         r = create_request(move_velocity, move_duration, self._last_platform_status, self.__compute_turning_point(angle))
         self._move_request_publisher.publish(r)
 
+    def _handle_platform_status(self, status:PlatformStatus):
+        self._last_platform_status = status
 
 def main():
     platform = PathPlatformController()
