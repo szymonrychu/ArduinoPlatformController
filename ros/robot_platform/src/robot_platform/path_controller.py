46b9647699445015ad04816f76122208dc1514f9
Szymon Richert <szymon.rychu@gmail.com>
2024-07-24 21:19:02 +0200

    reduce turning algorithm

--- ros/robot_platform/src/robot_platform/path_controller.py
+++ ros/robot_platform/src/robot_platform/path_controller.py
@@ -18,7 +18,7 @@ duration = 1.0
 TINY_ANGLE_DELTA = 0.05
 SMALL_ANGLE_DELTA = 0.2
 SLOW_SPEED = 0.2
-ROTATION_SPEED = math.pi
+ROTATION_SPEED = math.pi/(0.18*3)
 
 class PathPlatformController(ROSNode):
 
