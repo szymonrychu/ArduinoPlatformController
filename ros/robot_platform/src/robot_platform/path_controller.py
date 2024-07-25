75f96761adc3b5b3a28a20fcbed167f24a76ab4e
Szymon Richert <szymon.rychu@gmail.com>
2024-07-25 09:42:15 +0200

    Change way the angle is computed

--- ros/robot_platform/src/robot_platform/path_controller.py
+++ ros/robot_platform/src/robot_platform/path_controller.py
@@ -111,7 +111,7 @@ class PathPlatformController(ROSNode):
             r_in_place.motor2.velocity = 0
             r_in_place.motor3.velocity = 0
             r_in_place.motor4.velocity = 0
-            r_in_place.duration = ROTATION_SPEED * (abs_angle_delta/math.pi) # min servo turn duration
+            r_in_place.duration = ROTATION_SPEED * self._controller_frequency * (abs_angle_delta/math.pi) # min servo turn duration
             self.__send_request(r_in_place)
             time.sleep(r_in_place.duration) # wait until servos are fully turned
             self.__send_request(r) # send move forward request
