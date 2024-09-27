de24f6e878566fbb5122238ad0c2db232b028bd1
Szymon Richert <szymon.rychu@gmail.com>
2024-09-28 00:14:22 +0200

    fix missing serial issue

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+ROS_ROOT="${CONDA_PREFIX:-/opt/ros/noetic}"
+
 readonly CATKIN_WS_DIR="$(dirname "$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")")"
 cd "${CATKIN_WS_DIR}"
 
-source /opt/ros/noetic/setup.bash
+source "${CONDA_PREFIX}/setup.bash"
 
-catkin_make -DCMAKE_INSTALL_PREFIX=/opt/ros/noetic install
+catkin_make "-DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX}" install
