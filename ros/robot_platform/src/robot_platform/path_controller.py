a847f61b41a4d760061cb5c1a4080cbd9ec6d451
Szymon Richert <szymon.rychu@gmail.com>
2024-11-02 18:07:02 +0100

    feat: Simplify path_controller:

--- ros/robot_platform/src/robot_platform/path_controller.py
+++ ros/robot_platform/src/robot_platform/path_controller.py
@@ -11,7 +11,7 @@ from .ros_helpers import ROSNode
 from copy import deepcopy
 
 from robot_platform.msg import PlatformStatus, MoveRequest
-from geometry_msgs.msg import PoseArray, Twist
+from geometry_msgs.msg import PoseArray, Twist, Point
 from nav_msgs.msg import Odometry
 
 TINY_ANGLE_DELTA = math.pi/18
@@ -21,6 +21,9 @@ MAX_SPEED = 0.25
 ROTATION_SPEED = math.pi/0.8
 TINY_WAIT_S = 0.1
 CAN_MOVE_CONTINOUSLY_CTR_MAX = 5
+
+REQUEST_DURATION=0.1
+MAX_SERVO_ROTATION_ANGLE_WITHIN_REQUEST = PlatformStatics.TURN_VELOCITY * REQUEST_DURATION
 
 class PathPlatformController(ROSNode):
 
@@ -43,7 +46,6 @@ class PathPlatformController(ROSNode):
         self._move_request_publisher = rospy.Publisher(move_request_output_topic, MoveRequest)
         rospy.Subscriber(platform_status_input_topic, PlatformStatus, self._handle_platform_status)
         rospy.Subscriber('/cmd_vel', Twist, self._handle_trajectory_update)
-
         self.spin()
 
     def __send_request(self, r):
@@ -69,37 +71,48 @@ class PathPlatformController(ROSNode):
         return any(deltas_too_big)
 
     def _handle_trajectory_update(self, cmd_vel:Twist):
-        angle = 0
-        move_velocity = 0
+        requested_path_length = cmd_vel.linear.x/self._controller_frequency
+        turning_point = Point()
+        turning_point.x = requested_path_length/cmd_vel.angular.z
+
+        abs_move_velocity = min(max(abs(cmd_vel.linear.x), SLOW_SPEED), MAX_SPEED)
+        move_velocity = abs_move_velocity if cmd_vel.linear.x > 0 else -abs_move_velocity
+        
+        r = create_request(move_velocity, 1/self._controller_frequency + 0.5, self._last_platform_status, turning_point)
+        self.__send_request(r)
+
+
+        # angle = 0
+        # move_velocity = 0
     
-        if cmd_vel.linear.x > 0:
-            move_velocity = min(max(cmd_vel.linear.x, SLOW_SPEED), MAX_SPEED)
-            angle = cmd_vel.angular.z * 1.04
-        elif cmd_vel.linear.x < 0:
-            move_velocity = max(min(cmd_vel.linear.x, -SLOW_SPEED), -MAX_SPEED)
-            angle = cmd_vel.angular.z * 1.04
+        # if cmd_vel.linear.x > 0:
+        #     move_velocity = min(max(cmd_vel.linear.x, SLOW_SPEED), MAX_SPEED)
+        #     angle = cmd_vel.angular.z * 1.04
+        # elif cmd_vel.linear.x < 0:
+        #     move_velocity = max(min(cmd_vel.linear.x, -SLOW_SPEED), -MAX_SPEED)
+        #     angle = cmd_vel.angular.z * 1.04
             
 
-        abs_angle_delta = abs(angle - self._last_angle)
-        crossing_0_angle = (angle > 0 and self._last_angle < 0) or (angle < 0 and self._last_angle > 0) and abs_angle_delta > SMALL_ANGLE_DELTA
+        # abs_angle_delta = abs(angle - self._last_angle)
+        # crossing_0_angle = (angle > 0 and self._last_angle < 0) or (angle < 0 and self._last_angle > 0) and abs_angle_delta > SMALL_ANGLE_DELTA
         
 
-        angle_delta_tiny = abs_angle_delta < TINY_ANGLE_DELTA
-        moves_slowly = abs(move_velocity) == SLOW_SPEED
-        angle_tiny = abs(angle) < TINY_ANGLE_DELTA
-        changes_direction = (move_velocity < 0 and self._last_velocity > 0) or (move_velocity > 0 and self._last_velocity < 0)
+        # angle_delta_tiny = abs_angle_delta < TINY_ANGLE_DELTA
+        # moves_slowly = abs(move_velocity) == SLOW_SPEED
+        # angle_tiny = abs(angle) < TINY_ANGLE_DELTA
+        # changes_direction = (move_velocity < 0 and self._last_velocity > 0) or (move_velocity > 0 and self._last_velocity < 0)
 
-        self._last_angle = angle
-        self._last_velocity = move_velocity
+        # self._last_angle = angle
+        # self._last_velocity = move_velocity
 
 
-        turning_point = Point()
+        # turning_point = Point()
 
-        if angle != 0 and self._controller_frequency != 0:
-            turning_point.y = move_velocity / (angle * self._controller_frequency)
+        # if angle != 0 and self._controller_frequency != 0:
+        #     turning_point.y = move_velocity / (angle * self._controller_frequency)
         
-        r = create_request(move_velocity, 1.2, self._last_platform_status, turning_point)
-        self.__send_request(r)
+        # r = create_request(move_velocity, 1.2, self._last_platform_status, turning_point)
+        # self.__send_request(r)
 
         # if (angle_delta_tiny or angle_tiny) and not changes_direction:
         #     rospy.loginfo(f"Handling tiny turn without slowdown delta={abs_angle_delta}")
