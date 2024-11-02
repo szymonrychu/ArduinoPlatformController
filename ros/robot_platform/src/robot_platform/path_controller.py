7df99ac03a37b8257304f55f0f1f3558bad61d29
Szymon Richert <szymon.rychu@gmail.com>
2024-11-02 18:32:38 +0100

    feat: Simplify path_controller:

--- ros/robot_platform/src/robot_platform/path_controller.py
+++ ros/robot_platform/src/robot_platform/path_controller.py
@@ -73,7 +73,7 @@ class PathPlatformController(ROSNode):
     def _handle_trajectory_update(self, cmd_vel:Twist):
         abs_move_velocity = min(max(abs(cmd_vel.linear.x), SLOW_SPEED), MAX_SPEED)
         move_velocity = abs_move_velocity if cmd_vel.linear.x > 0 else -abs_move_velocity
-        abs_turn_radius = max(abs(cmd_vel.angular.z), PlatformStatics.ROBOT_WIDTH/2 + 0.01)
+        abs_turn_radius = 2.0 * max(abs(cmd_vel.angular.z), PlatformStatics.ROBOT_WIDTH/2 + 0.01)
         turn_radius = abs_turn_radius if cmd_vel.angular.z > 0 else -abs_turn_radius
         turn_radius = turn_radius if move_velocity > 0 else -turn_radius
 
