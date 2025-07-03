288174c8ed27118cf1a1ac3d8d75203481f807dd
Szymon Richert <szymon.rychu@gmail.com>
2025-07-03 22:41:50 +0200

    feat: Add Odometry filtering based on platform readiness

--- ros/robot_platform/src/robot_platform/wheel_controller.py
+++ ros/robot_platform/src/robot_platform/wheel_controller.py
@@ -148,7 +148,8 @@ class WheelController(ROSNode, SafeSerialWrapper):
         self._total_X += mean_distance_delta * math.cos(self._total_yaw)
         self._total_Y += mean_distance_delta * math.sin(self._total_yaw)
 
-        if all([m.ready for m in response.motor_list]) and timestamp > self._next_request_finish_time:
+        # if all([m.ready for m in response.motor_list]) and timestamp > self._next_request_finish_time:
+        if timestamp > self._next_request_finish_time:
             odometry = Odometry()
             odometry.header.stamp = rospy_time_now
             odometry.header.frame_id = self._base_frame_id
