0fb16594e1f5e4de1351b59da03ac4294982741d
Szymon Richert <szymon.rychu@gmail.com>
2025-07-04 22:21:50 +0200

    feat: Cleanup

---
+++ update_platformio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo nounset
+
+readonly CURRENT_PWD="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+sudo systemctl stop ros_robot_platform
+sleep 5
+
+platformio run -e small_robot -t upload
+
+sleep 5
+sudo systemctl start ros_robot_platform
---
+++ update_ros.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo nounset
+
+readonly CURRENT_PWD="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+sudo systemctl stop ros_robot_platform
+sleep 5
+
+cd ../..
+catkin_make "-DCMAKE_INSTALL_PREFIX=/opt/ros/noetic"
+
+sleep 5
+sudo systemctl start ros_robot_platform
