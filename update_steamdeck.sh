f589ecf3ae01776a5a39f180a8471ca182425c2c
Szymon Richert <szymon.rychu@gmail.com>
2025-07-05 07:16:41 +0200

    feat: Update for steamdeck refresh

--- update_steamdeck.sh
+++ update_steamdeck.sh
@@ -3,9 +3,7 @@
 ROS_ROOT="${CONDA_PREFIX:-/opt/ros/noetic}"
 
 readonly CATKIN_WS_DIR="$(dirname "$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")")"
-cd "${CATKIN_WS_DIR}"
+cd "/home/deck/catkin_ws"
 
 source "${CONDA_PREFIX}/setup.bash"
-
 catkin_make "-DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX}" install
-catkin_make "-DCMAKE_INSTALL_PREFIX=/opt/ros/noetic" install
