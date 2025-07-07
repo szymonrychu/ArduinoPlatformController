16300390180925793c20702fc38b18b1602e7b41
Szymon Richert <szymon.rychu@gmail.com>
2025-07-07 14:38:05 +0000

    feat: Update servo angle limiting code

--- update_ros.sh
+++ update_ros.sh
@@ -3,11 +3,11 @@ set -euo nounset
 
 readonly CURRENT_PWD="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-sudo systemctl stop ros_robot_platform
+sudo systemctl stop ros_robot_platform ros_core
 sleep 5
 
 cd ../..
 catkin_make "-DCMAKE_INSTALL_PREFIX=/opt/ros/noetic"
 
 sleep 5
-sudo systemctl start ros_robot_platform
+sudo systemctl start ros_robot_platform ros_core
