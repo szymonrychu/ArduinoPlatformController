d8b3e8b70a2b94f82dff867e730e3399e7cf3136
Szymon Richert <szymon.rychu@gmail.com>
2025-07-13 20:27:16 +0000

    feat: Simplify ssystemd service structure

--- update_platformio.sh
+++ update_platformio.sh
@@ -3,10 +3,10 @@ set -euo nounset
 
 readonly CURRENT_PWD="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-sudo systemctl stop ros_robot_platform ros_core
+sudo systemctl stop ros_robot_platform
 sleep 5
 
 platformio run -e small_robot -t upload
 
 sleep 5
-sudo systemctl start ros_robot_platform ros_core
+sudo systemctl start ros_robot_platform
